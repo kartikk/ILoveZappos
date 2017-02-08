2ee5c4842158d23608c86209eb0f254abf2cb3d1
Kartikk R Pandian <kartikk127@gmail.com>
2017-02-08 16:44:51 -0500

    Query based on text from SearchView

--- app/src/main/java/com/kartikk/zappos/ilovezappos/MainActivity.java
+++ app/src/main/java/com/kartikk/zappos/ilovezappos/MainActivity.java
@@ -1,51 +1,72 @@
 package com.kartikk.zappos.ilovezappos;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.google.gson.GsonBuilder;
 import com.kartikk.zappos.ilovezappos.models.ZapposModel;
 import com.kartikk.zappos.ilovezappos.util.Constants;
 import com.kartikk.zappos.ilovezappos.util.Endpoints;
+import com.kartikk.zappos.ilovezappos.util.Helper;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     final String TAG = MainActivity.class.getSimpleName();
     RecyclerView searchRecyclerView;
+    Endpoints endpoints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         searchRecyclerView = (RecyclerView) findViewById(R.id.recyclerView1);
-        Retrofit retrofit = new Retrofit.Builder().baseUrl(Constants.baseURL)
-                .addConverterFactory(GsonConverterFactory
-                        .create((new GsonBuilder()).create())).build();
-        Endpoints endpoints = retrofit.create(Endpoints.class);
-        Call<ZapposModel> call = endpoints.search("nike", Constants.authKey);
-        call.enqueue(new Callback<ZapposModel>() {
-            @Override
-            public void onResponse(Call<ZapposModel> call, Response<ZapposModel> response) {
-                Log.d(TAG, "Search success");
-                SearchRecyclerAdapter searchRecyclerAdapter = new SearchRecyclerAdapter(response.body());
-                searchRecyclerView.setAdapter(searchRecyclerAdapter);
-                searchRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-            }
-
-            @Override
-            public void onFailure(Call<ZapposModel> call, Throwable t) {
-                Log.d(TAG, "Search failure");
-            }
-        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.menu_search_icon);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (endpoints == null) {
+                    endpoints = Helper.getRetrofitEndpoints();
+                }
+                newText = newText.trim();
+                Call<ZapposModel> call = endpoints.search(newText, Constants.authKey);
+                call.enqueue(new Callback<ZapposModel>() {
+                    @Override
+                    public void onResponse(Call<ZapposModel> call, Response<ZapposModel> response) {
+                        Log.d(TAG, "Search success");
+                        SearchRecyclerAdapter searchRecyclerAdapter = new SearchRecyclerAdapter(response.body());
+                        searchRecyclerView.setAdapter(searchRecyclerAdapter);
+                        searchRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                    }
+
+                    @Override
+                    public void onFailure(Call<ZapposModel> call, Throwable t) {
+                        Log.d(TAG, "Search failure");
+                    }
+                });
+                return false;
+            }
+        });
+        return true;
+    }
 }
